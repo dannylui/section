4e176450892bab36e7e861fa21368c19b6240a4c
Dan Lui <danlui@gmail.com>
2024-01-16 12:06:37 -0500

    update to shear calcs

--- P1.py
+++ P1.py
@@ -125,25 +125,33 @@ def is_stiffened(Geom_Input: dict, Stiff_input: dict):
     else:
         print ("Check unstiffened capacity using 6.10.9.2")
         return False
-        
+
+
 def Shear_buckling_k (Geom_Input, Stiff_input):
     if not is_stiffened(Geom_Input, Stiff_input):
+        print("k value per AASHTO 6.10.9.2")
         return 5
     else:
+        print("Calculate k per AASHTO 6.10.9.3.2-7")
         return 5 + 5 / (Stiff_input['Spacing d0'] / Geom_Input['D_web']) ** 2
 
+# Calculate Shear-buckling resistance tot he shear yield Strength
 def ratio_C(Geom_Input, Stiff_input):
     D = Geom_Input['D_web']
     tw = Geom_Input['t_web']
     E = Geom_Input['E']
     Fyw = Geom_Input['fyw']
     k = Shear_buckling_k(Geom_Input, Stiff_input)
+    print("k=" + str(k))
 
     if D/tw <= 1.12 * (E * k / Fyw) ** 0.5:
+        print("Calculate C per AASHTO 6.10.9.3.2-4")
         return 1
     elif D/tw < 1.4* (E * k / Fyw) ** 0.5:
+        print("Calculate C per AASHTO 6.10.9.3.2-5")
         return 1.12 / (D / tw) * (E * k / Fyw) ** 0.5
     else:
+        print("Calculate C per AASHTO 6.10.9.3.2-6")
         return 1.57 / ((D / tw) ** 2) * (E * k / Fyw)
     
 
@@ -156,39 +164,46 @@ def capacity_Vn (Geom_Input, Stiff_input):
     bbf = Geom_Input['b_bf']
 
     C = ratio_C(Geom_Input, Stiff_input)
+    print("C=" + str(C))
 
     if Stiff_input['Panel'] == 'Interior':
         if (2 * D * tw)/(btf*ttf + bbf * tbf) <= 2.5:             #AASHTO 6.10.9.3.2-1
+            print("Calculate Vn per AASHTO 6.10.9.3.2-1")
             return Plastic_Shear_Vp(Geom_Input) * (C + 0.87 * (1-C)/(1+(Stiff_input['Spacing d0'] / Geom_Input['D_web'])**2)**0.5)
-        else:
-           return Plastic_Shear_Vp(Geom_Input) * (C + 0.87 * (1-C)/(1+(Stiff_input['Spacing d0'] / Geom_Input['D_web'])**0.5 + (Stiff_input['Spacing d0'] / Geom_Input['D_web'])))
+        else:  #AASHTO 6.10.9.3.2-8
+           print("Calculate Vn per AASHTO AASHTO 6.10.9.3.2-8")
+           return Plastic_Shear_Vp(Geom_Input) * (C + 0.87 * (1-C)/((1+(Stiff_input['Spacing d0'] / Geom_Input['D_web'])**2)**0.5 + (Stiff_input['Spacing d0'] / Geom_Input['D_web'])))
     else: 
-         return Plastic_Shear_Vp(Geom_Input) * C
+        return Plastic_Shear_Vp(Geom_Input) * C
 
-
+def check_Vu(Vu, Vn):
+    if Vu > Vn:
+        return "NG"
+    else:
+        return "OK"
 
 
 Stiffener_Input = {'Stiffener': 'yes',  # Should be yes/no only 
                    'Panel':'Interior',       # End/ Interior only
-                   'Spacing d0': 100}    # Spacing in inches
+                   'Spacing d0': 300}    # Spacing in inches
 
 Input = {'fyw':50, 'fyf':50, 'E':29000,'fc':4,
          'b_slab':50, 't_slab':8,
-         'b_tf':12, 't_tf':0.5, 
+         'b_tf':28, 't_tf':1.75, 
          'D_web':110, 't_web':0.75, 
-         'b_bf':12, 't_bf':0.5}
+         'b_bf':28, 't_bf':2}
 
 
 BmSect = pd.DataFrame({'Element':["Slab","Top Flange","Web","Bottom Flange"],
                        'Width': [Input['b_slab'], Input['b_tf'], Input['t_web'], Input['b_bf']],
                        'Thickness/Height':[Input['t_slab'],Input['t_tf'],Input['D_web'],Input['t_bf']]})
 
-print(Plastic_Shear_Vp(Input))
+# print(Plastic_Shear_Vp(Input))
 
-print(is_stiffened(Input, Stiffener_Input))
-print("k = " + str(Shear_buckling_k(Input, Stiffener_Input)))
+# print(is_stiffened(Input, Stiffener_Input))
+# print("k = " + str(Shear_buckling_k(Input, Stiffener_Input)))
 
-print("C=" + str(ratio_C(Input, Stiffener_Input)))
+# print("C=" + str(ratio_C(Input, Stiffener_Input)))
 print("Vn=" + str(capacity_Vn(Input, Stiffener_Input)))
 
 # xlFilename = "SectionProp.xlsx"
