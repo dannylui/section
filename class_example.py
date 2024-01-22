1b7367d0f510273a2e3d6c001d61a8a49cbd499f
Dan Lui <dannylui@users.noreply.github.com>
2024-01-21 22:47:02 -0500

    Create class_example.py

---
+++ class_example.py
@@ -0,0 +1,15 @@
+class Shape:
+	def __init__(self, geo_input):
+		self.something = get_input['something']
+
+	def draw(self):
+		print(f"I'm draing a {self.fsdfs} shape")
+
+	def draw_with_color(self):
+		self.draw()
+
+
+
+triangle = Shape('red')
+triangle.draw()
+
