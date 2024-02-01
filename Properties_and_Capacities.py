51fec0f92b2def4a16e5f82e22748747fea49a4b
Dan Lui <danlui@gmail.com>
2024-02-01 00:08:48 -0500

    Added output list

--- Properties_and_Capacities.py
+++ Properties_and_Capacities.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import math
 
-class Section_Properties():
+class Section_Properties:
     
     def __init__(self, Geom_Input):
         
@@ -34,6 +35,9 @@ class Section_Properties():
         self.Es = Geom_Input['E']
         self.Fc = Geom_Input['fc']
 
+        self.output_list = []
+
+    
 
     def Calc_Elastic_Prop(self, n: int = 8):
 
@@ -100,38 +104,61 @@ class Section_Properties():
         # print(Section)
         # print(Modulus)
         # print(Inertia)
+
+        self.output_list.extend([Section, Inertia, Modulus])
         
         return Section, Modulus, Inertia
 
     #AASHTO 6.10.2.1
     def chk_web_proportion(self):
+        self.output_list.append("Checking Web proportions per AASHTO 6.10.2.1")
         if self.has_long_stiff == 'no':
+            self.output_list.append(f"{self.D_web} / {self.t_web} = {self.D_web / self.t_web} <= 150")
             return self.D_web / self.t_web <= 150
         else:
+            self.output_list.append(f"{self.D_web} / {self.t_web} = {self.D_web / self.t_web}<= 300")
             return self.D_web / self.t_web <= 300
 
     #Calculating Vp
     def Calc_Plastic_Shear_Vp(self):
+        self.output_list.extend(["Calculating Vp", 0.58 * self.fyw  * self.D_web * self.t_web])
         return 0.58 * self.fyw  * self.D_web * self.t_web
     
     # returns true if stiffened, false if not. Assume no longitudinal stiffener
     def is_stiffened(self, Stiff_input: dict):
+
+        self.output_list.append("Check if the section is stiffened")
+        
         if Stiff_input['Panel'] == 'Interior' and Stiff_input['Spacing d0'] <= self.D_web * 3:
+
+            self.output_list.append("Section is stiffened, check capacity using 6.10.9.3")
+            
             print ("Check stiffened capacity using 6.10.9.3")
             return True
+        
         elif Stiff_input['Panel'] == 'End' and Stiff_input['Spacing d0'] <= self.D_web * 1.5:
+            
+            self.output_list.append("Section is stiffened, check capacity using 6.10.9.3.3")
+
             print ("Check stiffened capacity using 6.10.9.3.3")
+            
             return True
         else:
+            
+            self.output_list.append("Section is unstiffened, check capacity using 6.10.9.2")
+            
             print ("Check unstiffened capacity using 6.10.9.2")
             return False
 
     def Calc_Shear_buckling_k (self, Stiff_input):
+        self.output_list.append("Calculating Shear Buckling, k")
         if not self.is_stiffened(Stiff_input):
             print("k value per AASHTO 6.10.9.2")
+            self.output_list.append("Per AASHTO 6.10.9.2, k = 5")
             return 5
         else:
             print("Calculate k per AASHTO 6.10.9.3.2-7")
+            self.output_list.append(f"Per AASHTO 6.10.9.3.2-7, k = {5 + 5 / (Stiff_input['Spacing d0'] / self.D_web) ** 2}")
             return 5 + 5 / (Stiff_input['Spacing d0'] / self.D_web) ** 2
         
     # Calculate Shear-buckling resistance to the shear yield Strength
@@ -143,12 +170,15 @@ class Section_Properties():
         print("k=" + str(k))
 
         if self.D_web / self.t_web <= 1.12 * (self.Es * k / self.fyw) ** 0.5:
+            self.output_list.append("Calculate C per AASHTO 6.10.9.3.2-4, C = 1")
             print("Calculate C per AASHTO 6.10.9.3.2-4")
             return 1
         elif self.D_web / self.t_web < 1.4* (self.Es * k / self.fyw) ** 0.5:
+            self.output_list.append(f"Calculate C per AASHTO 6.10.9.3.2-5, C = {1.12 / (self.D_web / self.t_web) * (self.Es * k / self.fyw) ** 0.5}")
             print("Calculate C per AASHTO 6.10.9.3.2-5")
             return 1.12 / (self.D_web / self.t_web) * (self.Es * k / self.fyw) ** 0.5
         else:
+            self.output_list.append(f"Calculate C per AASHTO 6.10.9.3.2-6, C = {1.57 / ((self.D_web / self.t_web) ** 2) * (self.Es * k / self.fyw)}")
             print("Calculate C per AASHTO 6.10.9.3.2-6")
             return 1.57 / ((self.D_web / self.t_web) ** 2) * (self.Es * k / self.fyw)
         
@@ -161,12 +191,18 @@ class Section_Properties():
 
         print("C=" + str(C))
 
+        self.output_list.append("Calculating Nominal Shear Vn")
+
         if Stiff_input['Panel'] == 'Interior':
             if (2 * self.D_web * self.t_web)/(self.b_tf * self.t_tf + self.b_bf * self.t_bf) <= 2.5:             #AASHTO 6.10.9.3.2-1
                 print("Calculate Vn per AASHTO 6.10.9.3.2-1")
+                self.output_list.extend(["Calculate Vn per AASHTO 6.10.9.3.2-1",
+                                         self.Calc_Plastic_Shear_Vp() * (C + 0.87 * (1-C)/(1+(d0 / self.D_web)**2)**0.5)])
                 return self.Calc_Plastic_Shear_Vp() * (C + 0.87 * (1-C)/(1+(d0 / self.D_web)**2)**0.5)
             else:  #AASHTO 6.10.9.3.2-8
                 print("Calculate Vn per AASHTO AASHTO 6.10.9.3.2-8")
+                self.output_list.extend(["Calculate Vn per AASHTO AASHTO 6.10.9.3.2-8",
+                                         self.Calc_Plastic_Shear_Vp() * (C + 0.87 * (1-C)/((1+(d0 / self.D_web)**2)**0.5 + (d0 / self.D_web)))])
                 return self.Calc_Plastic_Shear_Vp() * (C + 0.87 * (1-C)/((1+(d0 / self.D_web)**2)**0.5 + (d0 / self.D_web)))
         else: 
             return self.Calc_Plastic_Shear_Vp() * C
@@ -175,7 +211,7 @@ class Section_Properties():
     # Calculating the plastic neutral axis for positive flexure
     # Ref AASHTO Table D6.1-1
     # conservative ignore rebars for now - to be implemented later *****
-    def PNA (self):
+    def calc_PNA (self):
         Ps = 0.85 * self.Fc * self.bslab * self.tslab        # Slab
         Pt = self.fybf * self.b_bf * self.t_bf              # Bottom Flange/ tension
         Pc = self.fytf * self.b_tf * self.t_tf              # Top Flange/ compression
@@ -255,7 +291,7 @@ class Section_Properties():
         else:
             print('Web check per 6.10.2.1 is NG')
         
-        Y_bar, _ , PNA_loc = self.PNA()
+        Y_bar, _ , PNA_loc = self.calc_PNA()
         Dcp = 0
         
         if PNA_loc == "Web":
@@ -267,7 +303,7 @@ class Section_Properties():
             print('Web slenderness check is NG')
         return  None
     
-    def yield_moment (self, Forces_input, n, eta):
+    def yield_moment_My (self, Forces_input, n, eta):
 
         _,S_NC,_ = self.Calc_Elastic_Prop(0)
         _,S_LT,_ = self.Calc_Elastic_Prop(3*n)
@@ -303,7 +339,7 @@ class Section_Properties():
     # Depth to PNA from top of deck
     def calc_Dp (self):
 
-        Y_bar,_,PNA_loc = self.PNA()
+        Y_bar,_,PNA_loc = self.calc_PNA()
 
         Dp = Y_bar
 
@@ -320,19 +356,46 @@ class Section_Properties():
         return self.calc_Dp() <= 0.42 * self.calc_total_depth_Dt()
     
     # AASHTO 6.10.7.1.2
-    def Calc_nominal_flex_Mn(self):
+    def Calc_nominal_flex_Mn(self, span='simp'): #Simple vs cont
 
-        _, Mp, _ = self.PNA()
+        _, Mp, _ = self.calc_PNA()
+
+        Mn = 0
 
         if self.check_compactness():
             if self.calc_Dp() <= 0.1 * self.calc_total_depth_Dt():
-                return Mp
+                Mn =  Mp
             else:
-                return Mp * (1.07 - 0.7 * self.calc_Dp() / self.calc_total_depth_Dt())
+                Mn = Mp * (1.07 - 0.7 * self.calc_Dp() / self.calc_total_depth_Dt())
             
-            # add conditions for continuous spans 
-
+            if span == "cont":
+                Mn = min(Mn, 1.3 * self.calc_hyb_factor_Rh() * self.yield_moment_My())
 
         else:
             print('write for non-compact sections')
 
+        return Mn
+    
+    def calc_hyb_factor_Rh(self):
+        if self.fyw >= max(self.fybf, self.fytf):
+            return 1
+        else:
+            #Provide calculations for this
+            return 0
+
+    
+    #AASHTO 6.10.8.2.3
+     
+    def calc_Lp(self):
+
+        return 1.0 * self.calc_rt_for_LTB() * math.sqrt(self.Es/self.fytf)
+
+
+    def calc_rt_for_LTB(self):
+
+        return self.b_tf / math.sqrt(12 * (1+ 1/3 * (self.calc_Dc() * self.t_web) / (self.b_tf * self.t_tf)))
+    
+    def calc_Dc(self):
+        
+        # Provide Calcs
+        return 36.96
---
+++ test.py
@@ -0,0 +1,49 @@
+# import pandas as pd
+from Properties_and_Capacities import Section_Properties as pac
+
+# Section = pd.DataFrame({'Element':["Slab","Top Flange","Web","Bottom Flange"],
+#                        'Width': [1, 2, 3, 4],
+#                        'Thickness/Height':[5,6, 7, 8]})
+
+# section2 = pd.DataFrame({'s':["Slab","Top Flange","Web"],
+#                        'dsfd': [1, 2, 3],
+#                        'sdfds/Height':[5,6, 7]})
+
+
+# listcomb = [Section, section2]
+
+# print(listcomb[0])
+
+
+# AASHTO 6.5.4.2
+Resist_factors_phi = {'phi_v':1.0,    # Shear
+                      'phi_f':1.0 }   # Flexure
+
+Stiffener_Input = {'Panel':'Interior',      # End/ Interior only
+                   'Spacing d0': 300}       # Spacing in inches
+  
+
+# Input materials and geometry
+Input = {'fyw':50, 'fy_tf':50, 'fy_bf':50, 'E':29000,
+         'fc':4,
+         'Long stiffener':'no', 'Trans stiffener': 'yes',      # Should be yes/no only 
+         'b_slab':114, 't_slab':9, 't_haunch':4, 
+         'b_tf':16, 't_tf':1.0, 
+         'D_web':69, 't_web':0.5, 
+         'b_bf':18, 't_bf':1.75}
+
+# Forces at section from analysis
+Forces = {'M_dc1': 2202*12, 'M_dc2': 335*12,'M_dw': 322*12,'M_LL': 12222}
+
+modular_ratio_n = 8       # modular ratio, Es/Ec
+
+Import_factor = 1     # importance factor, essential, etc
+
+newSect = pac(Input)
+
+newSect.Calc_Elastic_Prop(8)
+newSect.chk_web_proportion()
+newSect.Calc_nominal_shear_Vn(Stiffener_Input)
+for i in newSect.output_list:
+    print("====")
+    print(i)
